b16b851f2d4f09ad74673e0f475a24742ee6e145
Lakshay Aggarwal <lakshayagg29@gmail.com>
2024-12-24 04:22:21 +0530

    add about component

---
+++ src/components/contentContainer/ContentContainer.tsx
@@ -0,0 +1,11 @@
+import About from "./about/About";
+
+const ContentContainer = (): JSX.Element => {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+};
+
+export default ContentContainer;
